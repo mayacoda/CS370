23d5b4b353c313ce8a4fe01ab4481d4dfb2188ee
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2020-09-05 15:33:28 +0200

    Improve asset caching

--- demo/common/fern.ts
+++ demo/common/fern.ts
@@ -3,7 +3,7 @@ import {GameObject} from "../../engine-lib/data";
 export async function createFern() {
     const fern = new GameObject();
     await fern.loadObj('models/fern.obj', 'models/fern.mtl');
-    await fern.loadTransparentTexture('models/Fern Base Color.png');
+    await fern.loadTransparentTexture('models/Fern_Base_Color.png');
 
     return fern;
 }
--- demo/common/tree.ts
+++ demo/common/tree.ts
@@ -4,7 +4,7 @@ export async function createTree(): Promise<GameObject> {
     const tree = new GameObject();
 
     await tree.loadObj('models/tree.obj', 'models/tree.mtl');
-    await tree.loadTransparentTexture('models/Fern Base Color.png');
+    await tree.loadTransparentTexture('models/Fern_Base_Color.png');
 
     return tree;
 }
